e4fe8159dbfafbf28f3e3db865dab9e25592c362
Johnny Chen <149898820+Johnnynyc@users.noreply.github.com>
2024-11-26 21:54:25 -0500

    Update hand_all.ino

--- hand_all.ino
+++ hand_all.ino
@@ -38,26 +38,43 @@ void setup() {
 void loop() {
 
   int flexADC = analogRead(flexpin1);     //Reads voltage from A0, Max(1023)
-  Serial.println(flexADC);
+  Serial.println("Analog 1 reading: " + flexADC);   //Analog reading
   float flexV = flexADC * VCC / 1023.0;   //Voltage across flex sensor
   float flexR = R_DIV * (VCC / flexV - 1.0);    //Resistance of the flex sensor
-  Serial.println("Resistance: " + String(flexR) + " ohms");
+  Serial.println("Flex Sensor 1, Resistance: " + String(flexR) + " ohms");      //Flex sensor resistance reading
   int angle = map(flexADC, 0, 1023, 0, 180);    //map(value, lowest vol, highestvol, servo low, servo high)
   S1.write(angle);
 
+
   int flexADC2 = analogRead(flexpin2);
+  Serial.println("Analog 2 reading: " + flexADC2);   //Analog reading
+  float flexV2 = flexADC2 * VCC / 1023.0;   //Voltage across flex sensor
+  float flexR2 = R_DIV * (VCC / flexV2 - 1.0);    //Resistance of the flex sensor
+  Serial.println("Flex Sensor 2, Resistance: " + String(flexR2) + " ohms");      //Flex sensor resistance reading
   int angle2 = map(flexADC2, 0, 1023, 0, 180);    //map(value, lowest vol, highestvol, servo low, servo high)
   S2.write(angle2);
 
   int flexADC3 = analogRead(flexpin3);
+  Serial.println("Analog 3 reading: " + flexADC3);   //Analog reading
+  float flexV3 = flexADC3 * VCC / 1023.0;   //Voltage across flex sensor
+  float flexR3 = R_DIV * (VCC / flexV3 - 1.0);    //Resistance of the flex sensor
+  Serial.println("Flex Sensor 3, Resistance: " + String(flexR3) + " ohms");      //Flex sensor resistance reading
   int angle3 = map(flexADC3, 0, 1023, 0, 180);    //map(value, lowest vol, highestvol, servo low, servo high)
   S3.write(angle3);
 
   int flexADC4 = analogRead(flexpin4);
+  Serial.println("Analog 4 reading: " + flexADC4);   //Analog reading
+  float flexV4 = flexADC4 * VCC / 1023.0;   //Voltage across flex sensor
+  float flexR4 = R_DIV * (VCC / flexV4 - 1.0);    //Resistance of the flex sensor
+  Serial.println("Flex Sensor 4, Resistance: " + String(flexR4) + " ohms");      //Flex sensor resistance reading
   int angle4 = map(flexADC4, 0, 1023, 0, 180);    //map(value, lowest vol, highestvol, servo low, servo high)
   S4.write(angle4);
 
   int flexADC5 = analogRead(flexpin5);
+  Serial.println("Analog 5 reading: " + flexADC5);   //Analog reading
+  float flexV5 = flexADC5 * VCC / 1023.0;   //Voltage across flex sensor
+  float flexR5 = R_DIV * (VCC / flexV5 - 1.0);    //Resistance of the flex sensor
+  Serial.println("Flex Sensor 5, Resistance: " + String(flexR5) + " ohms");      //Flex sensor resistance reading
   int angle5 = map(flexADC5, 0, 1023, 0, 180);    //map(value, lowest vol, highestvol, servo low, servo high)
   S5.write(angle5);
 
